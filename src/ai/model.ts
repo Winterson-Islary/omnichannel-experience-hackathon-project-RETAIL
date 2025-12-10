7376b170d5fbf3c087dce999828570ee87aecafe
Winterson <u19cse1028@cit.ac.in>
2025-12-10 19:48:47 +0530

    added new model provided by groq api

--- src/ai/model.ts
+++ src/ai/model.ts
@@ -1,6 +1,18 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { ChatOpenAI } from "@langchain/openai";
 
-export const model = new ChatGoogleGenerativeAI({
-    model: "gemini-2.5-flash",
-    temperature: 0.7,
-});
+const MODELS = {
+    groq_model: new ChatOpenAI({
+        model: "openai/gpt-oss-120b", // or mixtral-8x7b, llama3-70b, etc.
+        apiKey: process.env.GROQ_API_KEY,
+        configuration: {
+            baseURL: "https://api.groq.com/openai/v1",
+        },
+        temperature: 0.7,
+    }),
+    gemini_model: new ChatGoogleGenerativeAI({
+        model: "gemini-3-pro-preview",
+        temperature: 0.7,
+    }),
+};
+export const model = MODELS.groq_model;
